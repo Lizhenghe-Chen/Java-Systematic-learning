21945905794e99909ba9769213c845ceb9053fe7
Lizhenghe Chen <sgzchenl@liverpool.ac.uk>
2022-09-08 21:20:42 +0100

    test VsCode

--- JavaSystemLearning/src/App.java
+++ JavaSystemLearning/src/App.java
@@ -1,4 +1,7 @@
 import UnityTest.*;
+/**Welecome to My Simple Java Learning Area,
+ * This project currently contains baisc Java Knowledge
+ */
 public class App {
     static MonoBehaviour testObj = new MonoBehaviour();
     public static void main(String[] args) throws Exception {
